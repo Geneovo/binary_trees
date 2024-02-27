01175ea931b30343d320ebc69aa8463454fad1c7
Geneovo <bodjuvwederhie@gmail.com>
2024-02-27 15:47:07 -0800

    Tree balance

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (0);
+}
+
+/**
+ * binary_tree_height - measures the height of s binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t lt = 0, rt = 0;
+
+		lt = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		rt = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((lt > rt) ? 1 : rt);
+	}
+	return (0);
+}
